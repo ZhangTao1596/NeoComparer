0b8377700ea364f959d946d2852782fd237c88b1
ZhangTao1596 <zhangtao@ngd.neo.org>
2023-02-08 10:36:40 +0800

    timer sync targe

--- BlockTimer.cs
+++ BlockTimer.cs
@@ -7,21 +7,37 @@ namespace NeoUtil
     {
         public static void Run(string[] args)
         {
-            SyncTime();
+            string host = "http://localhost:10332";
+            uint best = uint.MaxValue;
+            if (args.Length > 0)
+                host = args[0];
+            if (args.Length > 1)
+                best = uint.Parse(args[1]);
+            SyncTime(host, best);
         }
 
-        public static void SyncTime()
+        public static void SyncTime(string host, uint best)
         {
-            var client = new RpcClient(new Uri("http://localhost:20332"));
+            var client = new RpcClient(new Uri(host));
             uint count = 0;
+            uint equaled = 0;
             while (true)
             {
                 var c = client.GetBlockCountAsync().Result;
                 Console.WriteLine($"block count: {c} {DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss")}");
                 if (c == count)
                 {
-                    Console.WriteLine($"Synced. Time: {DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss")}");
-                    break;
+                    equaled++;
+                    if (equaled >= 3 && (best == uint.MaxValue || c == best))
+                    {
+                        Console.WriteLine($"Synced. Time: {DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss")}");
+                        break;
+                    }
+
+                }
+                else if (equaled != 0)
+                {
+                    equaled = 0;
                 }
                 count = c;
                 Thread.Sleep(15000);
